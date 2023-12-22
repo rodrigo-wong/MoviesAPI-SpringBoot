c8121e5daf7e466319570aa321496ae231b8a315
rodrigo-wong <wongmacr@gmail.com>
2023-12-22 02:42:47 -0500

    added .env

--- src/main/java/com/movies/movies/MoviesApplication.java
+++ src/main/java/com/movies/movies/MoviesApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		String mongoDatabase = dotenv.get("MONGO_DATABASE");
+		String mongoUri = dotenv.get("MONGO_URI");
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 	@GetMapping("/root")
